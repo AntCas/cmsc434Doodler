b120edf3a4d01dc7a35dd373a6fdc264d3eab18f
Anthony Castrio <anthonycastrio@Anthonys-Air.verizon.net>
2016-11-08 16:44:40 -0500

    remoed some unused code

--- app/src/main/java/me/castrio/doodler/MainActivity.java
+++ app/src/main/java/me/castrio/doodler/MainActivity.java
@@ -31,61 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-//        // Handle Color button
-//        Button buttonSetColor = (Button) findViewById(R.id.roundButtonColor);
-//        buttonSetColor.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                DoodleView doodleView = (DoodleView) findViewById(R.id.doodleView);
-////                doodleView.setColor(Color.GREEN);
-//
-//            }
-//        });
-
-//        // Handle Size button
-//        Button buttonSetSize = (Button) findViewById(R.id.roundButtonSize);
-//        buttonSetSize.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                DoodleView doodleView = (DoodleView) findViewById(R.id.doodleView);
-//                doodleView.setSize(5);
-//
-//            }
-//        });
-
-//        // Handle Opacity button
-//        Button buttonSetOpacity = (Button) findViewById(R.id.roundButtonOpacity);
-//        buttonSetOpacity.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                DoodleView doodleView = (DoodleView) findViewById(R.id.doodleView);
-//                doodleView.setOpacity(150);
-//            }
-//        });
-
-        SeekBar newSeekBar = (SeekBar) findViewById(R.id.seekBarNew);
-        newSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) { // finger on
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) { // finger off
-//                Context context = getApplicationContext();
-//                CharSequence text = "The seekbar value is " + seekBar.getProgress();
-//                Toast.
-
-
-            }
-        });
     }
 
     private int _penColorSaved = -1;
